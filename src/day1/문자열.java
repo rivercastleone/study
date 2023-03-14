204b7b8667018b95033538e42b5792fda4eed20e
rivercastleone <sssw123c@gmail.com>
2023-03-14 22:21:54 +0900

    문자열

---
+++ src/day1/문자열.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class 문자열 {
+    public static void main(String[] args){
+        System.out.println('생');
+
+        System.out.println("생활코딩");
+
+        System.out.println('생활코딩'); //문자열을 작은 따옴표로 감싸면 에러가 발생한다
+
+        System.out.println("생"); //한 글자도 문자열이 될 수 있기 때문에 에러가 발생하지는 않는다.
+    }
+    
+}
