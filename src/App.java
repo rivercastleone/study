8799a3521d2405d24acd78afc5735d532fe0b87c
rivercastleone <sssw123c@gmail.com>
2023-03-14 22:02:06 +0900

    test

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int a=1;
+        System.out.println("egoing said \"Welcome programming world\"");
     }
 }
