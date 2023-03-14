e76bcd018fcef8dd7425072ea1dcc2b457b5d238
rivercastleone <sssw123c@gmail.com>
2023-03-14 22:43:01 +0900

    변수

---
+++ src/day1/variable.java
@@ -0,0 +1,30 @@
+package day1;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.SystemMenuBar;
+
+public class variable{
+    public static void main(String[] args){
+        int a;
+        a=1;
+        System.out.println(a+1);
+
+        a=2;
+        System.out.println(a+1);
+        
+        double b=1.1;
+        System.out.println(b+1.1);
+        
+        b=2.1;
+        System.out.println(b+1.1);
+
+        String first = "asdf";
+        System.out.println(first+" "+"zxcv");
+
+        String c, d;
+        c= "asdf";
+        d=" zxcv";
+        System.out.println(c+d);
+
+    }
+    
+}
