c38f1fc9e0a42884d15326755fbb1ce31021ef6c
rivercastleone <sssw123c@gmail.com>
2023-03-14 22:30:10 +0900

    숫자

--- src/day1/숫자.java
+++ src/day1/숫자.java
@@ -1,6 +1,6 @@
 package day1;
 
-public class 숫자 {
+public class 숫자{
     public static void main(String[] args){
         System.out.println(1+2); // 3
 
@@ -10,5 +10,4 @@ public class 숫자 {
 
         System.out.println(6/2); //3 
     }
-    
 }
